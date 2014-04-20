e86410b927886ce7311676c3ce7747aac2b3dd04
Justin Liu <yuting1987@gmail.com>
2014-04-21 02:23:24 +0800

    completed ustream

--- service/parser.js
+++ service/parser.js
@@ -3,10 +3,14 @@ var http = require('http'),
 
 var channel = {
   youtube: [
-    'yuting1987'
+    'yuting1987',               // 宇庭
+    'indietaiwan',              // 大帝音地
+    'UC6BgDThjkr6sEOovgcrvXjQ'  // Pei-Che Chang
   ],
   ustream: [
-
+    '17831478',                 // Appendectomy Project 割闌尾計畫
+    'art1025',                  // 公投盟
+    'nonukestw'                 // 不要核四、五六運動
   ]
 }
 
@@ -19,13 +23,41 @@ async.map(channel.youtube, function(cid, cb) {
       });
       res.on('end', function() {
         body = JSON.parse(body);
-        if (body.feed) {
+        if (body.feed.entry) {
             for (var i = 0, len = body.feed.entry.length; i < len; i++) {
+                var vid = /videos\/([\w]+)/.exec(body.feed.entry[i].content.src)[1];
                 live.push({
                     title: body.feed.entry[i].title.$t,
-                    vid: /videos\/([\w]+)/.exec(body.feed.entry[i].content.src)[1]
+                    vid: vid,
+                    url: 'http://youtu.be/' + vid
                 })
             };
+        }
+
+        cb(null, live);
+      });
+    }).on('error', function(e) {
+        cb(null, []);
+    });
+}, function(err, results){
+    console.log(results);
+});
+
+async.map(channel.ustream, function(cid, cb) {
+    http.get('http://api.ustream.tv/json?subject=channel&uid=' + cid + '&command=getInfo', function(res) {
+      var body = '';
+      var live = [];
+      res.on('data', function(chunk) {
+        body += chunk;
+      });
+      res.on('end', function() {
+        body = JSON.parse(body);
+        if (body.results.status == 'live') {
+            live.push({
+                title: body.results.title,
+                vid: id,
+                url: 'http://www.ustream.tv/channel/' + id
+            });
         }
 
         cb(null, live);
