f212a3d99c1e114e859f76a4d850016bb29d0933
Justin Liu <yuting1987@gmail.com>
2014-04-21 16:54:20 +0800

    use setInterval

--- service/parser.js
+++ service/parser.js
@@ -28,85 +28,95 @@ twlive.auth('oFqJQgFQujCDzQno7a88OsD54tzOEHTIGs9ltlcS', function(error, result) 
 
 var live = [];
 
-async.series({
-    'youtube': function(cb){
-        async.map(channel.youtube, function(cid, cb) {
-            http.get('http://gdata.youtube.com/feeds/api/users/' + cid + '/live/events?v=2&status=active&alt=json', function(res) {
-              var body = '';
-              var active = [];
-              res.on('data', function(chunk) {
-                body += chunk;
-              });
-              res.on('end', function() {
-                body = JSON.parse(body);
-                if (body.feed.entry) {
-                    for (var i = 0, len = body.feed.entry.length; i < len; i++) {
-                        var vid = /videos\/([\w]+)/.exec(body.feed.entry[i].content.src)[1];
-                        active.push({
-                            title: body.feed.entry[i].title.$t,
-                            vid: vid,
-                            url: 'http://youtu.be/' + vid
-                        });
-                    };
-                }
-
-                cb(null, active);
-              });
-            }).on('error', function(e) {
-                cb(null, []);
-            });
-        }, function(err, results){
-            var item = [];
-            results.forEach(function(id){
-                id.forEach(function(active){
-                    item.push(active);
-                });
-            });
-            cb(null, item);
-        });
-    },
-    'ustream': function(cb){
-        async.map(channel.ustream, function(cid, cb) {
-            http.get('http://api.ustream.tv/json?subject=channel&uid=' + cid + '&command=getInfo', function(res) {
-              var body = '';
-              var live = [];
-              res.on('data', function(chunk) {
-                body += chunk;
-              });
-              res.on('end', function() {
-                body = JSON.parse(body);
-                if (body.results.status == 'live') {
-                    live.push({
-                        title: body.results.title,
-                        vid: id,
-                        url: 'http://www.ustream.tv/channel/' + id
-                    });
-                }
-
-                cb(null, live);
-              });
-            }).on('error', function(e) {
-                cb(null, []);
-            });
-        }, function(err, results){
-            var item = [];
-            results.forEach(function(id){
-                id.forEach(function(active){
-                    item.push(active);
-                });
-            });
-            cb(null, item);
-        });
+var running = false;
+fetch = function() {
+    if (running !== false) {
+        return ;
     }
-}, function (err, results) {
-    results['youtube'].forEach(function(active){
-        active.type = 'youtube';
-        live['y'+active.vid] = active;
+    running = true;
+    async.parallel({
+        'youtube': function(cb){
+            async.map(channel.youtube, function(cid, cb) {
+                http.get('http://gdata.youtube.com/feeds/api/users/' + cid + '/live/events?v=2&status=active&alt=json', function(res) {
+                  var body = '';
+                  var active = [];
+                  res.on('data', function(chunk) {
+                    body += chunk;
+                  });
+                  res.on('end', function() {
+                    body = JSON.parse(body);
+                    if (body.feed.entry) {
+                        for (var i = 0, len = body.feed.entry.length; i < len; i++) {
+                            var vid = /videos\/([\w]+)/.exec(body.feed.entry[i].content.src)[1];
+                            active.push({
+                                title: body.feed.entry[i].title.$t,
+                                vid: vid,
+                                url: 'http://youtu.be/' + vid
+                            });
+                        };
+                    }
+
+                    cb(null, active);
+                  });
+                }).on('error', function(e) {
+                    cb(null, []);
+                });
+            }, function(err, results){
+                var item = [];
+                results.forEach(function(id){
+                    id.forEach(function(active){
+                        item.push(active);
+                    });
+                });
+                cb(null, item);
+            });
+        },
+        'ustream': function(cb){
+            async.map(channel.ustream, function(cid, cb) {
+                http.get('http://api.ustream.tv/json?subject=channel&uid=' + cid + '&command=getInfo', function(res) {
+                  var body = '';
+                  var live = [];
+                  res.on('data', function(chunk) {
+                    body += chunk;
+                  });
+                  res.on('end', function() {
+                    body = JSON.parse(body);
+                    if (body.results.status == 'live') {
+                        live.push({
+                            title: body.results.title,
+                            vid: id,
+                            url: 'http://www.ustream.tv/channel/' + id
+                        });
+                    }
+
+                    cb(null, live);
+                  });
+                }).on('error', function(e) {
+                    cb(null, []);
+                });
+            }, function(err, results){
+                var item = [];
+                results.forEach(function(id){
+                    id.forEach(function(active){
+                        item.push(active);
+                    });
+                });
+                cb(null, item);
+            });
+        }
+    }, function (err, results) {
+        results['youtube'].forEach(function(active){
+            active.type = 'youtube';
+            live['y'+active.vid] = active;
+        });
+        results['ustream'].forEach(function(active){
+            active.type = 'ustream'
+            live['u'+active.vid] = active;
+        });
+        console.log(new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + ' ' + live.length);
+        twlive.child('live').set(live);
+        running = false;
     });
-    results['ustream'].forEach(function(active){
-        active.type = 'ustream'
-        live['u'+active.vid] = active;
-    });
-    console.log(live);
-    twlive.child('live').set(live);
-});
+}
+
+setInterval('fetch', 5 * 60 * 1000);
