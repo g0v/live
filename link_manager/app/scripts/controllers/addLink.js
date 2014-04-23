f981ff42f864a501c557a4946551f51094a7abfe
Justin Lee <lis186@gmail.com>
2014-04-23 16:58:35 +0800

    UI tweak
    Signed-off-by: Justin Lee <lis186@gmail.com>

--- link_manager/app/scripts/controllers/addLink.js
+++ link_manager/app/scripts/controllers/addLink.js
@@ -2,7 +2,17 @@
 
 angular.module('liveApp')
   .controller('AddLinkCtrl', function ($scope, $firebase, md5) {
-    
+
+    $scope.prompt = '已收集的連結';
+
+    $scope.updatePrompt = function(){
+      if($scope.linkSearch.$.length == 0){
+        $scope.prompt = '已收集的連結';
+      }else{
+        $scope.prompt = '網址相似的連結';
+      }
+    }
+
     var linkRef = new Firebase("https://livelink-submission.firebaseio.com/links/");
     $scope.links = $firebase(linkRef);
 
@@ -29,7 +39,13 @@ angular.module('liveApp')
     }
 
     $scope.addLink = function() {
-      $scope.links.$add({url: $scope.linkSearch.$, added_at: new Date().getTime()});
-      $scope.addAlert('success', '謝謝您提供的資訊！');
+      if($scope.is_valid_url($scope.linkSearch.$)){
+        $scope.links.$add({url: $scope.linkSearch.$, added_at: new Date().getTime()});
+        $scope.addAlert('success', '我們已經收到網址'+$scope.linkSearch.$);
+        $scope.linkSearch.$ = '';
+        $scope.prompt = '已收集的連結';
+      }else{
+        $scope.addAlert('danger', '網址格式有誤！');
+      }
     }
   });
