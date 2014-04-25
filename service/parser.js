24814a687b8540cb7567b8dca230fdd11a3686e1
Justin Liu <yuting1987@gmail.com>
2014-04-26 03:08:41 +0800

    add new_live data

--- service/parser.js
+++ service/parser.js
@@ -2,12 +2,16 @@ var http = require('http'),
     async = require('async'),
     fs = require('fs');
 var Firebase = require('firebase');
+var mongodb = require('mongodb')
+  , MongoClient = mongodb.MongoClient;
+var time = require('time');
 
 if ( !fs.existsSync('database.json') ) {
     fs.linkSync('database-sample.json', 'database.json');
 }
 
 var database = JSON.parse(fs.readFileSync('./database.json', 'utf8').replace(/\/\/[ \S]*/gi,''));
+var now = new time.Date().setTimezone('Asia/Taipei');
 
 console.log(database);
 
@@ -21,15 +25,69 @@ db_firebase.auth(database.token, function(error, result) {
   }
 });
 
-var live;
-var channel = database.channel;
+var fetch = {
+    'youtube': function(id, cb) {
+        http.get('http://gdata.youtube.com/feeds/api/users/' + id + '/live/events?v=2&status=active&alt=json', function(res) {
+          var body = '';
+          var active = [];
+          res.on('data', function(chunk) {
+            body += chunk;
+          });
+          res.on('end', function() {
+            body = JSON.parse(body);
+            if (body.feed.entry) {
+                for (var i = 0, len = body.feed.entry.length; i < len; i++) {
+                    var vid = /videos\/([\w]+)/.exec(body.feed.entry[i].content.src)[1];
+                    active.push({
+                        type: 'youtube',
+                        title: body.feed.entry[i].title.$t,
+                        vid: vid,
+                        url: 'http://youtu.be/' + vid
+                    });
+                };
+            }
 
-var running = false;
-fetch = function() {
-    if (running !== false) {
-        return ;
+            cb(null, active);
+          });
+        }).on('error', function(e) {
+            cb(null, []);
+        });
+    },
+    'ustream': function(id, cb){
+        http.get('http://api.ustream.tv/json?subject=channel&uid=' + id + '&command=getInfo', function(res) {
+          var body = '';
+          var live = [];
+          res.on('data', function(chunk) {
+            body += chunk;
+          });
+          res.on('end', function() {
+            body = JSON.parse(body);
+            if (body.results.status == 'live') {
+                live.push({
+                    type: 'ustream',
+                    title: body.results.title,
+                    vid: body.results.id,
+                    url: 'http://www.ustream.tv/channel/' + body.results.id
+                });
+            }
+
+            cb(null, live);
+          });
+        }).on('error', function(e) {
+            cb(null, []);
+        });
     }
-    running = true;
+}
+
+MongoClient.connect(database.channel, function(err, db) {
+
+  var collection = db.collection('channel')
+
+  collection.find({}).toArray(function(err, docs) {
+    if (err) {
+      return console.error(err)
+    }
+
     async.parallel({
         'database': function(cb){
             db_firebase.child('live').once('value', function(live) {
@@ -41,103 +99,58 @@ fetch = function() {
                 cb(null, db || {});
             })
         },
-        'youtube': function(cb){
-            async.map(channel.youtube, function(cid, cb) {
-                http.get('http://gdata.youtube.com/feeds/api/users/' + cid + '/live/events?v=2&status=active&alt=json', function(res) {
-                  var body = '';
-                  var active = [];
-                  res.on('data', function(chunk) {
-                    body += chunk;
-                  });
-                  res.on('end', function() {
-                    body = JSON.parse(body);
-                    if (body.feed.entry) {
-                        for (var i = 0, len = body.feed.entry.length; i < len; i++) {
-                            var vid = /videos\/([\w]+)/.exec(body.feed.entry[i].content.src)[1];
-                            active.push({
-                                title: body.feed.entry[i].title.$t,
-                                vid: vid,
-                                url: 'http://youtu.be/' + vid
-                            });
-                        };
-                    }
-
-                    cb(null, active);
-                  });
-                }).on('error', function(e) {
-                    cb(null, []);
-                });
-            }, function(err, results){
-                var item = [];
-                results.forEach(function(id){
-                    id.forEach(function(active){
-                        item.push(active);
-                    });
-                });
-                cb(null, item);
-            });
-        },
-        'ustream': function(cb){
-            async.map(channel.ustream, function(cid, cb) {
-                http.get('http://api.ustream.tv/json?subject=channel&uid=' + cid + '&command=getInfo', function(res) {
-                  var body = '';
-                  var live = [];
-                  res.on('data', function(chunk) {
-                    body += chunk;
-                  });
-                  res.on('end', function() {
-                    body = JSON.parse(body);
-                    if (body.results.status == 'live') {
-                        live.push({
-                            title: body.results.title,
-                            vid: body.results.id,
-                            url: 'http://www.ustream.tv/channel/' + body.results.id
-                        });
-                    }
-
-                    cb(null, live);
-                  });
-                }).on('error', function(e) {
-                    cb(null, []);
-                });
-            }, function(err, results){
-                var item = [];
-                results.forEach(function(id){
-                    id.forEach(function(active){
-                        item.push(active);
-                    });
-                });
-                cb(null, item);
+        'live': function(cb){
+            async.map(docs, function(item, cb){
+                if ( fetch[item.type] ) {
+                    fetch[item.type](item.id, cb);
+                }
+            }, function (err, results) {
+                cb(null, results);
             });
         }
     }, function (err, results) {
         var count = 0;
-        var now = Math.floor(Date.now() / 1000);
-        live = results['database'] || {};
-        results['youtube'].forEach(function(active){
-            active.type = 'youtube';
-            active.status = 'live';
-            active.updated_at = now;
-            live['y'+active.vid] = active;
-            count += 1;
-        });
-        results['ustream'].forEach(function(active){
-            active.type = 'ustream'
-            active.updated_at = now;
-            active.status = 'live';
-            live['u'+active.vid] = active;
-            count += 1;
+        var updated_at = Math.floor(Date.now() / 1000);
+        var live = results['database'] || {};
+        var new_live = [];
+
+        results['live'].forEach(function(source){
+            source.forEach(function(active){
+                var name = (active.type=='youtube' ? 'y' : 'u')+active.vid;
+                if ( !live[name] ) {
+                    live[name] = {};
+                    new_live.push(active);
+                }
+                for (key in active) {
+                    live[name][key] = active[key];
+                }
+                live[name]['status'] = 'live';
+                live[name].updated_at = updated_at;
+                count += 1;
+            });
         });
         for (key in live)
         {
-            if ( (live[key].updated_at + 15 * 60) < now ) {
+            if ( (live[key].updated_at + 15 * 60) < updated_at ) {
                 delete live[key];
             }
         }
-        console.log(new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + ' ' + count);
-        db_firebase.child('live').set(live);
+        console.log(now.toISOString().replace(/T/, ' ').replace(/\..+/, '') + ' ' + count);
+        console.log(live);
+        console.log(new_live);
+        // db_firebase.child('live').set(live);
         running = false;
     });
-}
+  });
+});
 
-setInterval(fetch, 5 * 60 * 1000);
+// var running = false;
+// fetch = function() {
+//     if (running !== false) {
+//         return ;
+//     }
+//     running = true;
+    
+// }
+
+// setInterval(fetch, 5 * 60 * 1000);
